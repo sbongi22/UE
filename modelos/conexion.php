50e89d69ec1e4ceb2386a2667746456de7c4c1fb
bongi22 <sbongi22@gmail.com>
2026-02-06 15:39:56 -0300

    correccion en conexion

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -2,10 +2,10 @@
 class Conexion{
     static public function conectar(){
         // EJEMPLO con datos REALES de InfinityFree
-        $host = "universidad-edifier.infinityfreeapp.com"; // TU host real
+        $host = "sql105.infinityfree.com"; // TU host real
         $dbname = "if0_41092513_db_universidad"; // TU base de datos
         $user = "if0_41092513"; // TU usuario
-        $pass = "I5mwDD7kwI9"; // TU password real
+        $pass = "NuevaPass2026"; // TU password real
         
         try {
             $link = new PDO("mysql:host=$host;dbname=$dbname", 
